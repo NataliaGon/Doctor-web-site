3a4e0ba979de30abb37a17c96ff65a754d1b705c
NataliaGon <gon4arovanatalija@gmail.com>
2017-10-12 21:54:09 +0300

    Page3 and Page4

---
+++ page4.js
@@ -0,0 +1,20 @@
+var questions = document.querySelectorAll('.question');
+for (var i = 0; i < NavLinks.length; i++) {
+    var navLink = NavLinks[i];
+    // navLink.addEventListener('click', function (event) {
+    // 		var allNavs = document.querySelectorAll('.nav-link div');
+    //     for (var it = 0; it < allNavs.length; it++){
+    //     	console.log(allNavs[it]);
+    //       allNavs[it].classList.add('invisible');
+    //       allNavs[it].classList.remove('circleVisible');
+    //      }
+    //     console.log(allNavs);
+    // 		var targetElement = event.target || event.srcElement;
+    //     var circleDiv = targetElement.parentNode.querySelectorAll('div');
+        console.log('123');
+    //     circleDiv[0].classList.add('circleVisible');
+    //     circleDiv[0].classList.remove('invisible');
+    //     console.log(circleDiv[0]);
+    }
+    );  
+  }

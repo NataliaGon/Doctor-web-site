e65df52c710269ecc81c2c6dcbe923ec158444e2
NataliaGon <gon4arovanatalija@gmail.com>
2017-10-25 15:58:19 +0300

    new version

--- page4.js
+++ page4.js
@@ -1,40 +1,18 @@
-var questions = document.querySelectorAll('.question');
-for (var i = 0; i < questions.length; i++) {
-    var question = questions[i];
-    question.addEventListener('click', function (event) {
-                  
-            
-            var targetElement = event.target;
 
-            var allAnswers = targetElement.parentNode.querySelectorAll('.answer');           
-            if (allAnswers[0].style.display=="block"){
-                allAnswers[0].style.display="none";
-            }
-            else { 
-            allAnswers[0].style.display="block";
-            }
-            
+var acc = document.getElementsByClassName("accordion");
 
-            var allCaretsLeft = targetElement.parentNode.querySelectorAll('.icon-caret-left');
-            if (allCaretsLeft[0].style.display=="none"){
-                allCaretsLeft[0].style.display="block";
-            }
-            else { 
-                allCaretsLeft[0].style.display="none";
-            }
-                
-           
 
-            var allCaretsDown = targetElement.parentNode.querySelectorAll('.icon-caret-down');
-            if (allCaretsDown[0].style.display=="block"){
-                allCaretsDown[0].style.display="none";
-            }
-            else { 
-                allCaretsDown[0].style.display="block";
-            }
-    
-            
+for (var i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        
+        this.classList.toggle("active");
+
+        
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
         }
-    
-    );
+    }
 }
